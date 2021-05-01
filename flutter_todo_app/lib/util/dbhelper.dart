3eadeea1d5072588bcf45e1a0f4cd35e5aa83ff7
hakananit <hkn.anit@gmail.com>
2021-05-01 17:20:34 +0300

    chore: initilize db

--- flutter_todo_app/lib/util/dbhelper.dart
+++ flutter_todo_app/lib/util/dbhelper.dart
@@ -12,11 +12,30 @@ class DbHelper {
   String colDate = "date";
   String colPriority = "priority";
 
+  static Database _db;
+
+  Future<Database> get db async {
+    if (_db == null) {
+      _db = await initializeDb();
+    }
+    return _db;
+  }
+
   DbHelper._internal();
 
   factory DbHelper() {
     return _dbHelper;
   }
 
-  
+  Future<Database> initializeDb() async {
+    Directory dir = await getApplicationDocumentsDirectory();
+    String path = dir.path + "todos.db";
+    var dbTodos = await openDatabase(path, version: 1, onCreate: _createDb);
+    return dbTodos;
+  }
+
+  void _createDb(Database db, int newVersion) async {
+    await db.execute(
+        "CREATE TABLE $tblTodo($colId INTEGER PRIMARY KEY, $colTitle TEXT, $colDescription TEXT, $colPriority TEXT, $colPriority INTEGER,$colDate TEXT");
+  }
 }
