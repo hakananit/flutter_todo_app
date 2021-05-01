5a50790ca59ec1b3b6dbe4fb3d99d49b9a1d2c60
hakananit <hkn.anit@gmail.com>
2021-05-01 17:05:34 +0300

    chore: update deps and add dbhelper

---
+++ flutter_todo_app/lib/util/dbhelper.dart
@@ -0,0 +1,22 @@
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+class DbHelper {
+  static final DbHelper _dbHelper = DbHelper._internal();
+  String tblTodo = "todo";
+  String colId = "id";
+  String colTitle = "title";
+  String colDescription = "description";
+  String colDate = "date";
+  String colPriority = "priority";
+
+  DbHelper._internal();
+
+  factory DbHelper() {
+    return _dbHelper;
+  }
+
+  
+}
